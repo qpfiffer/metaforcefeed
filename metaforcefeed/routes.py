39883cffc87e61d9aa51ec5ed1a0c4548a91abd2
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-04-30 18:10:34 -0700

    MORE STUFF

--- metaforcefeed/routes.py
+++ metaforcefeed/routes.py
@@ -1,4 +1,4 @@
-from flask import g, current_app, Blueprint, render_template
+from flask import g, request, current_app, Blueprint, render_template, redirect
 
 from metaforcefeed.cache import ol_view_cache
 
@@ -8,5 +8,22 @@ app = Blueprint('metaforcefeed', __name__, template_folder='templates')
 @app.route("/", methods=['GET'])
 @ol_view_cache
 def root():
-    items = g.db.get("all_items") or []
+    #TODO: Refactor this when prefix matching is done.
+    all_items = g.db.get("all_items") or []
+
+    for item_key in all_items:
+        pass
+
+    return render_template("index.html")
+
+@app.route("/submit", methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        return redirect(url_for('metaforcefeed.root'))
+    return render_template("index.html")
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return redirect(url_for('metaforcefeed.root'))
     return render_template("index.html")
