af33c10128b46c0d36037c3132e6f1f7db9b684c
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-07-13 14:15:29 -0700

    More design stuff.

--- metaforcefeed/static/js/ping.js
+++ metaforcefeed/static/js/ping.js
@@ -7,13 +7,11 @@ function ping_object(e) {
     httpRequest.setRequestHeader("Content-Type", "application/json");
     httpRequest.onreadystatechange = function() {
         if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-            console.log("Response: ", httpRequest.responseText);
             var resp_json = JSON.parse(httpRequest.responseText);
             var error_ele = dom_ele.parentElement.getElementsByClassName("error")[0];
             if (resp_json.success) {
                 var pings_ele = dom_ele.parentElement.getElementsByClassName("pings")[0];
-                var pings_ele = dom_ele.parentElement.getElementsByClassName("pings")[0];
-                pings_ele.textContent = resp_json.ping_obj.pings;
+                pings_ele.textContent = resp_json.ping_obj.pings + " ✈";
                 error_ele.textContent = "";
                 dom_ele.onclick = function() { return true; };
             } else {
@@ -29,7 +27,12 @@ function ping_object(e) {
 
 function ping_init() {
     all_pingable = document.getElementsByClassName("arrow_up");
+    also_pingable = document.getElementsByClassName("small_arrow_up");
+    // Don't even fucking care. Fuck JS.
     for (var i = 0; i < all_pingable.length; i++) {
         all_pingable[i].onclick = ping_object;
+    }
+    for (var i = 0; i < also_pingable.length; i++) {
+        also_pingable[i].onclick = ping_object;
     }
 }
