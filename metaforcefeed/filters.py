568a75ed53d93719d87ced9c689714ca3e661ba7
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-08-03 14:14:34 -0700

    Prettier user badges.

--- metaforcefeed/filters.py
+++ metaforcefeed/filters.py
@@ -1,9 +1,12 @@
 # -*- coding: UTF-8 -*-
 from datetime import datetime
 
+badges = [u"☹", u"♞", u"★", u"☎", u"☂"]
+
 def unix_to_human(timestamp_str):
     time = float(timestamp_str)
     return unicode(datetime.fromtimestamp(time))
 
 def user_badge(username):
-    return u"☹"
+    username_hash = hash(username)
+    return u'<span class="user_badge">{}</span>'.format(badges[username_hash % len(badges)])
