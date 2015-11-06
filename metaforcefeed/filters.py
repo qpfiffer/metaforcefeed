849343a3737ca8200b1e19e262e6f6019063133f
Quinlan Pfiffer <qpfiffer@gmail.com>
2015-11-05 23:19:47 -0800

    Fix syntax error

--- metaforcefeed/filters.py
+++ metaforcefeed/filters.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 from datetime import datetime
 
-badges = [u"☹", u"♞", u"★", u"☎", u"☂", u"📱", u"📴", u"📶",, u"📼", u"🔀"]
+badges = [u"☹", u"♞", u"★", u"☎", u"☂", u"📱", u"📴", u"📶", u"📼", u"🔀"]
 colors = [u"FF667F", u"FF9966", u"FFE666", u"CCFF66", u"66CCFF"]
 
 def unix_to_human(timestamp_str):
